b912a1c7e2310f135af7c67ef19949431919cf36
YevhenKharchenko <evgenvalerych@gmail.com>
2024-04-05 12:36:04 +0300

    update

--- src/components/Mission.jsx
+++ src/components/Mission.jsx
@@ -1,6 +1,9 @@
-import React from "react";
+import { useLocation } from "react-router-dom";
 
 const Mission = () => {
+  const location = useLocation();
+  console.log(location);
+
   return <div>Mission</div>;
 };
 
