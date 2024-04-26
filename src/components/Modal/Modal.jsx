98b8b6685f9f923539ecd3ae91d3876f65494ba3
YevhenKharchenko <evgenvalerych@gmail.com>
2024-04-26 11:31:27 +0300

    updated

--- src/components/Modal/Modal.jsx
+++ src/components/Modal/Modal.jsx
@@ -1,5 +1,53 @@
 import css from "./Modal.module.scss";
 
+const items = [
+  {
+    id: 1,
+    title: "Мед квітковий натуральний",
+    image: "../../../src/assets/honey.jpg",
+    volume: "0,25",
+    quantity: 1,
+    price: 100,
+  },
+  {
+    id: 2,
+    title: "Мед з волоськимим горіхами",
+    image: "../../../src/assets/honey.jpg",
+    volume: "0,25",
+    quantity: 3,
+    price: 200,
+  },
+  {
+    id: 3,
+    title: "Мед акацієвий натуральний",
+    image: "../../../src/assets/honey.jpg",
+    volume: "0,25",
+    quantity: 2,
+    price: 100,
+  },
+  {
+    id: 4,
+    title: "Мед ріпаковий натуральний",
+    image: "../../../src/assets/honey.jpg",
+    volume: "0,25",
+    quantity: 4,
+    price: 500,
+  },
+  {
+    id: 5,
+    title: "Мед соняшниковий натуральний",
+    image: "../../../src/assets/honey.jpg",
+    volume: "0,25",
+    quantity: 1,
+    price: 250,
+  },
+];
+
+const totalPrice = items.reduce((acc, el) => {
+  return acc + el.price;
+}, 0);
+console.log(totalPrice);
+
 function Modal({ handleModal }) {
   return (
     <div className={css.backdrop}>
@@ -9,7 +57,7 @@ function Modal({ handleModal }) {
           type="button"
           onClick={handleModal}
         >
-          close
+          X
           {/* <svg className={css.modalCloseIcon} width="28" height="28">
             <use href="./images/icons.svg#icon-x-close"></use>
           </svg> */}
@@ -17,38 +65,39 @@ function Modal({ handleModal }) {
         <h2 className={css.modalTitle}>Ваш кошик</h2>
         <div className={css.listContainer}>
           <ul className={css.list}>
-            <li className={css.listItem}>
-              <div className={css.imgWrapper}>
-                <img
-                  className={css.itemImg}
-                  src="../../../src/assets/honey.jpg"
-                  alt=""
-                  width={80}
-                  height={80}
-                />
-              </div>
-              <div className={css.descriptionWrapper}>
-                <h3 className={css.itemTitle}>Мед квітковий натуральний</h3>
-                <p className={css.itemVolume}>0,25 л</p>
-                <div className={css.itemQuantityWrapper}>
-                  <p className={css.itemQuantityText}>Кількість</p>
-                  <button className={css.quantityBtn} type="button">
-                    -
-                  </button>
-                  <p>1</p>
-                  <button className={css.quantityBtn} type="button">
-                    +
-                  </button>
-                </div>
-                <p className={css.itemPrice}>100 грн</p>
-                <button className={css.deleteBtn} type="button">
-                  del
-                </button>
-              </div>
-            </li>
-            <li className={css.listItem}></li>
-            <li className={css.listItem}></li>
-            <li className={css.listItem}></li>
+            {items.map((item) => {
+              return (
+                <li key={item.id} className={css.listItem}>
+                  <div className={css.imgWrapper}>
+                    <img
+                      className={css.itemImg}
+                      src={item.image}
+                      alt={item.title}
+                      width={80}
+                      height={80}
+                    />
+                  </div>
+                  <div className={css.descriptionWrapper}>
+                    <h3 className={css.itemTitle}>{item.title}</h3>
+                    <p className={css.itemVolume}>{item.volume} л</p>
+                    <div className={css.itemQuantityWrapper}>
+                      <p className={css.itemQuantityText}>Кількість</p>
+                      <button className={css.quantityBtn} type="button">
+                        -
+                      </button>
+                      <p>{item.quantity}</p>
+                      <button className={css.quantityBtn} type="button">
+                        +
+                      </button>
+                    </div>
+                    <p className={css.itemPrice}>{item.price} грн</p>
+                    <button className={css.deleteBtn} type="button">
+                      del
+                    </button>
+                  </div>
+                </li>
+              );
+            })}
           </ul>
         </div>
         <div className={css.modalBottomWrapper}>
@@ -58,8 +107,7 @@ function Modal({ handleModal }) {
             </a>
           </div>
           <div className={css.modalSubmitWrapper}>
-            <p className={css.totalPrice}>250 грн</p>
-
+            <p className={css.totalPrice}>{totalPrice} грн</p>
             <button className={css.modalSubmitBtn}>Оформити замовлення</button>
           </div>
         </div>
